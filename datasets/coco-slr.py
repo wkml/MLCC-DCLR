602ec29bf211eef4f33c8546c630c6819b7dfd1f
WiKim <1260050422@qq.com>
2023-04-13 22:15:36 +0800

    'coco-slr'

---
+++ datasets/coco-slr.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append(os.path.join( os.path.dirname(os.path.abspath(__file__)), '..', 'cocoapi/PythonAPI'))
+sys.path.append(os.path.join( os.path.dirname(os.path.abspath(__file__)), '..'))
+
+import json
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.utils.data as data
+import torchvision.datasets as datasets
+
+from pycocotools.coco import COCO
