8f4c8702b895cc4096546273dc142d9fdc7af19c
WiKim <1260050422@qq.com>
2023-04-19 15:31:33 +0800

    'coco-slr'

--- datasets/coco-slr.py
+++ datasets/coco-slr.py
@@ -5,7 +5,9 @@ sys.path.append(os.path.join( os.path.dirname(os.path.abspath(__file__)), '..'))
 
 import json
 import numpy as np
-from PIL import Image
+from PIL import Image,ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 
 import torch
 import torch.utils.data as data
@@ -29,35 +31,35 @@ class COCO_SLR(data.Dataset):
         self.coco = COCO(anno_path)
         self.ids = list(self.coco.imgs.keys())
 
-        with open('/data1/2022_stu/wikim_exp/mlp-pl/data/coco/category.json','r') as load_category:
+        with open('/home/horace/workspace/Wikim/MLP-PL-master/data/coco/category.json','r') as load_category:
             self.category_map = json.load(load_category)
 
         # labels : numpy.ndarray, shape->(len(coco), 80)
         # value range->(-1 means label don't exist, 1 means label exist)
         self.labels = []
         self.images = []
+        self.locas = []
         for i in range(len(self.ids)):
             img_id = self.ids[i]
             ann_ids = self.coco.getAnnIds(imgIds=img_id)
             target = self.coco.loadAnns(ann_ids)
-            category_ids, locas = getCategoryList(target)
+            category_ids, bboxs = getCategoryList(target)
 
-            path = self.coco.loadImgs(img_id)[0]['file_name']
-            input = Image.open(os.path.join(self.root, path)).convert('RGB')
-
-            for j in range(len(locas)):
-                self.images.append(input.crop(locas[j]))
+            for j in range(len(bboxs)):
+                self.images.append(img_id)
+                self.locas.append(bboxs[j])
                 self.labels.append(getLabelVector(category_ids[j], self.category_map))
 
         self.labels = np.array(self.labels)
-        self.labels[self.labels == 0] = -1
 
     def __getitem__(self, index):
-
-        input = self.images[index]
+        img_id = self.images[index]
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+        input = Image.open(os.path.join(self.root, path)).convert('RGB')
+        input = input.crop(self.locas[index])
         if self.input_transform:
             input = self.input_transform(input)
-        return index, input, self.changedLabels[index], self.labels[index]
+        return index, input, self.labels[index]
 
     def __len__(self):
         return len(self.images)
@@ -66,8 +68,10 @@ def getCategoryList(item):
     categories = []
     locas = []
     for t in item:
+        x, y, w, h = t['bbox']
+        x1, y1, x2, y2 = x, y, x+w, y+h
         categories.append(t['category_id'])
-        locas.append(t['bbox'])
+        locas.append([x1, y1, x2, y2])
     return categories, locas
 
 
